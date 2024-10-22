8ca04d8fa704d12ff21b1d84a1eec6093d79272d
nguyentran-edu <congchicken@gmail.com>
2024-10-22 23:38:26 +0700

    Update congcu_lv3.js

--- TieuHoc/js/congcu_lv3.js
+++ TieuHoc/js/congcu_lv3.js
@@ -315,7 +315,7 @@ function renderToWebsite1(data){
                                     + "<td class=\"table-data-form-real-estate-CongVu\" style=\"text-align:center;\">"
                                     + "<a href=\"#info" + String(count+1) + "\" onclick=\"goToLink("
                                     + "\'"
-                                    + String(data[count]['linktodo'])
+                                    + String(data[count]['linktodo1'])
                                     + "\'"
                                     + ",\'"
                                     + String(data[count]['id'])
@@ -361,7 +361,7 @@ function renderToWebsite2(data){
                                     + "<td class=\"table-data-form-real-estate-CongVu\" style=\"text-align:center;\">"
                                     + "<a href=\"#info" + String(count+1) + "\" onclick=\"goToLink("
                                     + "\'"
-                                    + String(data[count]['linktodo'])
+                                    + String(data[count]['linktodo1'])
                                     + "\'"
                                     + ",\'"
                                     + String(data[count]['id'])
